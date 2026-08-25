d38a048329b99bfd8f6270d26b40bdcbe3e3bcde
TagReaper <coxlucas2022@gmaill.com>
2026-08-25 15:44:29 -0500

    Created and Compiled Test C Program

---
+++ Systems-Programming/prog1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //including standard I/O
+
+// main function
+int main(){
+    //Printing to terminal
+    printf("Hello, World!\n");
+    
+    //Finished Program
+    return 0;
+}
