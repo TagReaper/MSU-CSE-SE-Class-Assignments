75bc54d23648c6dd8254c59d1eec28090d428b48
TagReaper <coxlucas2022@gmaill.com>
2026-09-03 16:01:17 -0500

    Created an LS directory call

--- Systems-Programming/Extra Programs/9-3-26/main.c
+++ Systems-Programming/Extra Programs/9-3-26/main.c
@@ -4,16 +4,37 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#include <dirent.h>
+
 
 int checkError(int val, const char * msg){
     if (val == -1){
         perror(msg);
-        exit(EXIT_FAULURE);
+        exit(EXIT_FAILURE);
     }
     return val;
 }
 
 int main(){
-    return 1;
+
+    struct dirent *dir;
+    DIR * dirp;
+
+    dirp = opendir(".");
+    if (dirp == NULL) {
+        checkError(-1, "failed to open directory");
+    }
+
+    while ((dir = readdir(dirp)) != NULL){
+        printf("%s\n", dir->d_name);
+    }
+
+    if (errno != 0) {
+        checkError(-1, "failed to open directory");
+    }
+
+    closedir(dirp);
+
+    return 0;
 }
 
